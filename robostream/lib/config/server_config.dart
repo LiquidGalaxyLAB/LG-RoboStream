e2fa41c27ae257117680ea59c80c3219c6f0e62d
Alejandro <9302714@alumnos.ufv.es>
2025-07-04 18:28:28 +0200

    Implement code changes to enhance functionality and improve performance

--- robostream/lib/config/server_config.dart
+++ robostream/lib/config/server_config.dart
@@ -1,5 +1,5 @@
 class ServerConfig {
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://10.0.2.2:8000';
   
   static const Duration requestTimeout = Duration(seconds: 5);
   static const Duration updateInterval = Duration(seconds: 1);
